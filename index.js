f47b605217971ce58a00e6e54fe55538c9c7c40a
rithi <ritimahesh29@gmail.com>
2021-12-27 12:27:55 +0530

    new commi3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("server connected");
+  res.status(200).send("server connected");
 });
 app.use("/", router);
 
